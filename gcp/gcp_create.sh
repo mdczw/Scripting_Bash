c597bc57974a29d7585c38877c9575c6cab22d70
Rita <mdyczewska@griddynamics.com>
2023-12-22 13:53:09 +0100

    Code formatting

--- gcp/gcp_create.sh
+++ gcp/gcp_create.sh
@@ -3,10 +3,9 @@
 #Pushes a java spring-petlinic container image built earlier to Artifacts Registry
 #Runs container on the VM.
 #
-#Usage: 
+#Usage:
 #Run <./gcp_create.sh [OPTIONS...]> to create resources and run container.
 #Example: ./gcp_create.sh -d <Dockerfile> -I <Allowed_IPs>
-
 
 PROJECT_ID="gd-gcp-internship-devops"
 REGION="us-central1"
@@ -23,35 +22,34 @@ DOCKERFILE_PATH=""
 CONTAINER_NAME="cloud-spring-petclinic"
 DOCKER_IMAGE="$REGION-docker.pkg.dev/$PROJECT_ID/${BASE_RESOURS_NAME}ar/${CONTAINER_NAME}"
 
-
 createEnvironment() {
 
 	gcloud compute networks create "$BASE_RESOURS_NAME"vpc \
-	    --project="$PROJECT_ID" \
-	    --subnet-mode=custom 
+		--project="$PROJECT_ID" \
+		--subnet-mode=custom
 
 	gcloud compute networks subnets create "$BASE_RESOURS_NAME"subnet \
-	    --project="$PROJECT_ID" \
-	    --range="$SUBNET_RANGE" \
-	    --network="$BASE_RESOURS_NAME"vpc \
-	    --region="$REGION"
+		--project="$PROJECT_ID" \
+		--range="$SUBNET_RANGE" \
+		--network="$BASE_RESOURS_NAME"vpc \
+		--region="$REGION"
 
 	gcloud compute --project="$PROJECT_ID" firewall-rules create "$BASE_RESOURS_NAME"firewall-rules \
-	    --network="$BASE_RESOURS_NAME"vpc \
-	    --allow="$ALLOWED_PORTS" \
-	    --target-tags=sp \
+		--network="$BASE_RESOURS_NAME"vpc \
+		--allow="$ALLOWED_PORTS" \
+		--target-tags=sp \
 		--source-ranges="$ALLOWED_IP"
 
 	gcloud compute addresses create "$BASE_RESOURS_NAME"static-ip \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION"
+		--project="$PROJECT_ID" \
+		--region="$REGION"
 }
 
 pushImage() {
 
 	gcloud artifacts repositories create "$BASE_RESOURS_NAME"ar \
-	    --repository-format=docker \
-	    --location="$REGION"
+		--repository-format=docker \
+		--location="$REGION"
 
 	docker build -t "$CONTAINER_NAME" "$DOCKERFILE_PATH"
 
@@ -59,92 +57,92 @@ pushImage() {
 
 	gcloud auth configure-docker "$REGION"-docker.pkg.dev
 
-	docker push "$DOCKER_IMAGE"	
+	docker push "$DOCKER_IMAGE"
 }
 
 runContainer() {
 
 	STATIC_IP_SP=$(gcloud compute addresses describe "$BASE_RESOURS_NAME"static-ip \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION" \
-	    --format='value(address)')
+		--project="$PROJECT_ID" \
+		--region="$REGION" \
+		--format='value(address)')
 
 	gcloud compute instances create-with-container "$BASE_RESOURS_NAME"vm \
-	    --project="$PROJECT_ID" \
-	    --zone="$ZONE" \
-	    --machine-type=e2-small \
-	    --subnet="$BASE_RESOURS_NAME"subnet \
-	    --tags=sp \
-	    --image=projects/cos-cloud/global/images/cos-101-17162-336-28 \
-	    --address="$STATIC_IP_SP" \
-	    --container-image="$DOCKER_IMAGE":latest \
-	    --container-arg="--server.port=80"
+		--project="$PROJECT_ID" \
+		--zone="$ZONE" \
+		--machine-type=e2-small \
+		--subnet="$BASE_RESOURS_NAME"subnet \
+		--tags=sp \
+		--image=projects/cos-cloud/global/images/cos-101-17162-336-28 \
+		--address="$STATIC_IP_SP" \
+		--container-image="$DOCKER_IMAGE":latest \
+		--container-arg="--server.port=80"
 }
 
 main() {
 
 	if [[ $DOCKERFILE_PATH=="" ]]; then
-	  	echo ""
+		echo ""
 		echo "Specify path to Docker file"
 		echo ""
 		echo "Usage example:"
-        echo "$0 -d <Dockerfile> -I <Allowed_IPs>"
-	    echo "Run $0 -h for more information"
+		echo "$0 -d <Dockerfile> -I <Allowed_IPs>"
+		echo "Run $0 -h for more information"
 		exit 1
 	fi
-    createEnvironment
-    pushImage
-    runContainer
+	createEnvironment
+	pushImage
+	runContainer
 }
 
 while getopts "r:b:s:I:P:d:c:h" flag; do
-  case $flag in
-    r)
-      REGION=$OPTARG
-      ;;
-    b)
-      BASE_RESOURS_NAME=$OPTARG
-      ;;
-    s)
-      SUBNET_RANGE=$OPTARG
-      ;;
-    I)
-      ALLOWED_IP=$OPTARG
-      ;;
-    P)
-      ALLOWED_PORTS=$OPTARG
-      ;;
-    d)
-      DOCKERFILE_PATH=$OPTARG
-	  if [[ ! -e $DOCKERFILE_PATH ]]; then
-	  	echo "Specify path to Docker file"
+	case $flag in
+	r)
+		REGION=$OPTARG
+		;;
+	b)
+		BASE_RESOURS_NAME=$OPTARG
+		;;
+	s)
+		SUBNET_RANGE=$OPTARG
+		;;
+	I)
+		ALLOWED_IP=$OPTARG
+		;;
+	P)
+		ALLOWED_PORTS=$OPTARG
+		;;
+	d)
+		DOCKERFILE_PATH=$OPTARG
+		if [[ ! -e $DOCKERFILE_PATH ]]; then
+			echo "Specify path to Docker file"
+			exit 1
+		fi
+		;;
+	c)
+		CONTAINER_NAME=$OPTARG
+		;;
+	h)
+		echo ""
+		echo "[-r] to set up REGION (default us-central1)"
+		echo "[-b] to set up BASE_RESOURS_NAME (default md-internship-sp-)"
+		echo "[-s] to set up SUBNET_RANGE (default 10.0.2.0/24)"
+		echo "[-I] to set up ALLOWED_IP (default 0.0.0.0/0)"
+		echo "[-P] to set up ALLOWED_PORTS (default tcp:80,tcp:22)"
+		echo "[-d] to set up DOCKERFILE_PATH"
+		echo "[-c] to set up CONTAINER_NAME (default cloud-spring-petclinic)"
+		echo ""
+		exit 0
+		;;
+	*)
+		echo ""
+		echo "Please use the correct syntax! Usage example:"
+		echo "$0 -d <Dockerfile> -I <Allowed_IPs>"
+		echo "Run $0 -h for more information"
+		echo ""
 		exit 1
-	  fi
-      ;;
-    c)
-      CONTAINER_NAME=$OPTARG
-      ;;
-	h)	
-      echo ""
-      echo "[-r] to set up REGION (default us-central1)"
-	  echo "[-b] to set up BASE_RESOURS_NAME (default md-internship-sp-)"
-	  echo "[-s] to set up SUBNET_RANGE (default 10.0.2.0/24)"
-	  echo "[-I] to set up ALLOWED_IP (default 0.0.0.0/0)"
-	  echo "[-P] to set up ALLOWED_PORTS (default tcp:80,tcp:22)"
-	  echo "[-d] to set up DOCKERFILE_PATH"
-	  echo "[-c] to set up CONTAINER_NAME (default cloud-spring-petclinic)"
-      echo ""
-      exit 0
-      ;;
-    *)
-      echo ""
-      echo "Please use the correct syntax! Usage example:"
-      echo "$0 -d <Dockerfile> -I <Allowed_IPs>"
-	  echo "Run $0 -h for more information"
-      echo ""
-      exit 1
-      ;;
-  esac
+		;;
+	esac
 done
 
 main
--- gcp_script.sh
+++ gcp_script.sh
@@ -3,10 +3,9 @@
 #Pushes a java spring-petlinic container image built earlier to Artifacts Registry
 #Runs container on the VM.
 #
-#Usage: 
+#Usage:
 #Run <source ./gcp_script.sh; start> to create resources and run container.
 #Run <source ./gcp_script.sh; remove> to remove all resources.
-
 
 PROJECT_ID="gd-gcp-internship-devops"
 REGION="us-central1"
@@ -15,40 +14,39 @@ DOCKERFILE_PATH="/Users/margaritadyczewska/Documents/DevOps_Course/spring-petcli
 DOCKER_IMAGE="$REGION-docker.pkg.dev/$PROJECT_ID/md-internship-sp-ar/cloud-spring-petclinic"
 ALLOWED_IP="89.64.40.117"
 
-
 createEnvironment() {
 
 	#Create VPC
 	gcloud compute networks create md-internship-sp-vpc \
-	    --project="$PROJECT_ID" \
-	    --subnet-mode=custom 
+		--project="$PROJECT_ID" \
+		--subnet-mode=custom
 
 	#Create subnet
 	gcloud compute networks subnets create md-internship-sp-subnet \
-	    --project="$PROJECT_ID" \
-	    --range=10.0.2.0/24 \
-	    --network=md-internship-sp-vpc \
-	    --region="$REGION"
+		--project="$PROJECT_ID" \
+		--range=10.0.2.0/24 \
+		--network=md-internship-sp-vpc \
+		--region="$REGION"
 
 	#Create firewall rules
 	gcloud compute --project="$PROJECT_ID" firewall-rules create md-internship-sp-firewall-rules \
-	    --network=md-internship-sp-vpc \
-	    --allow=tcp:80,tcp:22 \
-	    --target-tags=sp \
+		--network=md-internship-sp-vpc \
+		--allow=tcp:80,tcp:22 \
+		--target-tags=sp \
 		--source-ranges="$ALLOWED_IP"
 
-	#Reserve static IP address 
+	#Reserve static IP address
 	gcloud compute addresses create md-internship-sp-static-ip \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION"
+		--project="$PROJECT_ID" \
+		--region="$REGION"
 }
 
 pushImage() {
 
 	#Create artifacts repository
 	gcloud artifacts repositories create md-internship-sp-ar \
-	    --repository-format=docker \
-	    --location="$REGION"
+		--repository-format=docker \
+		--location="$REGION"
 
 	docker build -t cloud-spring-petclinic $DOCKERFILE_PATH
 
@@ -56,75 +54,74 @@ pushImage() {
 
 	gcloud auth configure-docker "$REGION"-docker.pkg.dev
 
-	docker push $DOCKER_IMAGE	
+	docker push $DOCKER_IMAGE
 }
 
 runContainer() {
 
 	#Get static IP address
 	STATIC_IP_SP=$(gcloud compute addresses describe md-internship-sp-static-ip \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION" \
-	    --format='value(address)')
+		--project="$PROJECT_ID" \
+		--region="$REGION" \
+		--format='value(address)')
 
 	#Create instances with container
 	gcloud compute instances create-with-container md-internship-sp-vm \
-	    --project="$PROJECT_ID" \
-	    --zone="$ZONE" \
-	    --machine-type=e2-small \
-	    --subnet=md-internship-sp-subnet \
-	    --tags=sp \
-	    --image=projects/cos-cloud/global/images/cos-101-17162-336-28 \
-	    --address="$STATIC_IP_SP" \
-	    --container-image="$DOCKER_IMAGE":latest \
-	    --container-arg="--server.port=80"
+		--project="$PROJECT_ID" \
+		--zone="$ZONE" \
+		--machine-type=e2-small \
+		--subnet=md-internship-sp-subnet \
+		--tags=sp \
+		--image=projects/cos-cloud/global/images/cos-101-17162-336-28 \
+		--address="$STATIC_IP_SP" \
+		--container-image="$DOCKER_IMAGE":latest \
+		--container-arg="--server.port=80"
 }
 
-
 deleteImage() {
-	
+
 	#Delete Docker image
 	gcloud artifacts docker images delete "$DOCKER_IMAGE" --quiet
 
 	#Delete artifacts repository
 	gcloud artifacts repositories delete md-internship-sp-ar \
-	    --project="$PROJECT_ID" \
-	    --location="$REGION" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--location="$REGION" \
+		--quiet
 }
 
 deleteInstances() {
 
 	#Delete instances
 	gcloud compute instances delete md-internship-sp-vm \
-	    --project="$PROJECT_ID" \
-	    --zone="$ZONE" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--zone="$ZONE" \
+		--quiet
 }
 
 deleteEnvironment() {
 
 	#Delete static IP address
 	gcloud compute addresses delete md-internship-sp-static-ip \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--region="$REGION" \
+		--quiet
 
 	#Delete firewall rules
 	gcloud compute firewall-rules delete md-internship-sp-firewall-rules \
-	    --project="$PROJECT_ID" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--quiet
 
 	#Delete subnet
 	gcloud compute networks subnets delete md-internship-sp-subnet \
-	    --project="$PROJECT_ID" \
-	    --region="$REGION" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--region="$REGION" \
+		--quiet
 
 	#Delete VPC
 	gcloud compute networks delete md-internship-sp-vpc \
-	    --project="$PROJECT_ID" \
-	    --quiet
+		--project="$PROJECT_ID" \
+		--quiet
 }
 
 start() {
